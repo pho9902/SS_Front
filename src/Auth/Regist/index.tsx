58c01966f0e2da40170a4225d9c438bfcb31625c
woo <pho9902@naver.com>
2024-11-01 13:59:15 +0900

    회원가입 페이지 더미데이터 생성

--- src/Auth/Regist/index.tsx
+++ src/Auth/Regist/index.tsx
@@ -1,3 +1,49 @@
+import * as S from "./style";
+import { useRef, useState } from "react";
+
 export default function Regist() {
-  return <div>회원가입</div>;
+  const sDummy = {
+    id: "aa1234",
+    pw: "as33",
+  };
+  const fDummy = [
+    {
+      id: "a",
+      pw: "as33",
+      // 아이디가 짧음 최소 2자
+    },
+    {
+      id: "aasdfkjasdoifhjdsiofh123",
+      pw: "as33",
+      //아이디 김 최대 8자
+    },
+  ];
+
+  const [isIdWrong, setIsIdWrong] = useState<boolean>(false);
+  const idRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <S.Wrap>
+      <S.Title>회원가입</S.Title>
+      <S.FormBox>
+        <S.Label>ID</S.Label>
+        <S.Input ref={idRef}></S.Input>
+      </S.FormBox>
+      <S.FormBox>
+        <S.Label>PassWord</S.Label>
+        <S.Input ref={passwordRef}></S.Input>
+      </S.FormBox>
+      <div>주소</div>
+      <S.BtnBox>
+        <S.Btn
+          onClick={() => {
+            console.log(idRef.current?.value, passwordRef.current?.value);
+          }}
+        >
+          회원가입
+        </S.Btn>
+      </S.BtnBox>
+    </S.Wrap>
+  );
 }
