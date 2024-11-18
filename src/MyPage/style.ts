a82d7cd1fdd7477b7e3abc4a99e1e7d3f2ff4030
woo <pho9902@naver.com>
2024-11-18 17:54:38 +0900

    feat: 마이페이지 레이아웃

---
+++ src/MyPage/style.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+    
+`;
+
+export const ProfileImg = styled.div`
+    border: 1px solid;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+`
+export const HeaderBox = styled.div`
+    display: flex;
+`
+
+export const ProfileId = styled.h2`
+
+`
+export const InfoBox = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+export const CoinImg = styled.div`
+    border: 1px solid;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    display: flex;	  
+    justify-content : center;
+    align-items: center;
+`
+export const CoinLeft = styled.span`
+
+`
+export const CoinAddBtn = styled.button`
+
+`
+
+export const CoinLeftBox = styled.div`
+    display: flex;
+`
+
+export const GetList = styled.div``
