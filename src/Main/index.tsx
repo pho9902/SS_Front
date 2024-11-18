750b7b01f5d662c6ffb8856ffb835474dfee0c83
woo <pho9902@naver.com>
2024-11-18 17:29:10 +0900

    feat: 메인 페이지 레이아웃

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,5 +1,19 @@
 import * as S from "./style";
 
 export default function Main() {
-  return <S.Wrap></S.Wrap>;
+  return <S.Wrap>
+    <S.HeaderBox>프로젝트명?</S.HeaderBox>
+    <S.BoxWrapper>
+      <S.IntroBox>coin</S.IntroBox>
+      <S.IntroBox>게임</S.IntroBox>
+    </S.BoxWrapper>
+    <S.BoxWrapper>
+      <S.IntroBox>문의</S.IntroBox>
+      <S.IntroBox>경품리스트</S.IntroBox>
+    </S.BoxWrapper>
+
+    <S.ExplainBox>
+      여기다 게임 설명 글 or 이미지 
+    </S.ExplainBox>
+  </S.Wrap>;
 }
--- src/Main/style.ts
+++ src/Main/style.ts
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
 export const Wrap = styled.div`
-  border: 1px solid red;
 `;
+
+export const HeaderBox = styled.div`
+  border: 1px solid blue;
+  width: 150px;
+  height: 50px;
+  margin-bottom: 150px
+`
+
+export const BoxWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 150px
+`
+export const IntroBox = styled.div`
+  width: 80px;
+  height: 40px;
+  border: 1px solid green;
+`
+
+export const ExplainBox = styled.div`
+  height: 500px;
+`
