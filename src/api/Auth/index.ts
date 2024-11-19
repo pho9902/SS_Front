5064002e7bed7c48c0e4f3fa58182b09bdd030d2
woo <pho9902@naver.com>
2024-11-19 09:33:18 +0900

    test: url 경로 테스트

--- src/api/Auth/index.ts
+++ src/api/Auth/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export function login(id: string, password: string) {
+  console.log(process.env.REACT_APP_DOMAIN + '/auth/login')
     return axios({
         url: process.env.REACT_APP_DOMAIN + '/auth/login',
         method: 'POST',
