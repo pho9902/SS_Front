d9aa91aee50836d4bdb3f3492e5885bb8ac2f57b
woo <pho9902@naver.com>
2024-11-20 15:33:48 +0900

    feat: id 이슈 해결

--- src/api/Auth/index.ts
+++ src/api/Auth/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export function login(id: string, password: string) {
+export function login(username: string, password: string) {
     return axios({
         url: process.env.REACT_APP_DOMAIN + '/auth/login',
         method: 'POST',
@@ -9,7 +9,7 @@ export function login(id: string, password: string) {
             'Content-Type': 'application/json'
           },
           data: {
-            id,
+            username,
             password
           }
     })
